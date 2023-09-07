382e1ce23aba5ae0e1c8f82e67805c5411d072b0
iiekezie <iiekezie@gmail.com>
2023-09-07 04:46:26 -0700

    Alx Low Level Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char *message = "Programming is like building a multilingual puzzle";
-
-    puts(message);
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
