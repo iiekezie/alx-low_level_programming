ba80064094f34ba4175718d1fc6520f28943141a
iiekezie <iiekezie@gmail.com>
2023-10-07 02:38:03 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef main_H
+#define main_H
+
+/**
+ * File: main.h
+ * Auth: Ifeanyi I. Ekezie
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0C.C-more_malloc_free directory.
+ */
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
