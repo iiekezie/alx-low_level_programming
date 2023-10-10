2da0bf1b42eb26c759c2db56124474915eeecdaf
iiekezie <iiekezie@gmail.com>
2023-10-10 10:07:39 -0700

    C - Preprocessor

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -6,13 +6,14 @@
 #include <stdio.h>
 
 /**
- * main - Write a Program Prints the name of the file of the program
- *        was compiled from, followed by a new line.
+ * main - Prints the name of the file it was compiled from,
+ *        followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    printf(__FILE__ "\n");
-    return (0);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
