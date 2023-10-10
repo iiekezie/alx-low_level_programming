586a39235b2fa904765559c462ad589507ea01ea
iiekezie <iiekezie@gmail.com>
2023-10-10 11:11:06 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+/*
+ * File: 5-free_dog.c
+ * Auth: Ifeanyi I Ekezie
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated for a dog structure.
+ * @d: Pointer to the dog structure to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
