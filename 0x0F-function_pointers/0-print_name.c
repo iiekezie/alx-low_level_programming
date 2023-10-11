39494001ec4fb06277b8fda0b51d09eeecaa86c5
iiekezie <iiekezie@gmail.com>
2023-10-11 04:18:45 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Ifeanyi I Ekezie
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
