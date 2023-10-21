f20eb5b82f826086345862701ada5157f3fd78d2
iiekezie <iiekezie@gmail.com>
2023-10-21 08:11:32 -0700

    #advanced

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,9 @@
-section .data
-  hello db "Hello, Holberton", 10  ; The string to be printed, followed by a newline character
-  hello_len equ $ - hello           ; Calculate the length of the string
-
-section .text
-  global _start
-
-_start:
-  ; Call printf
-  mov rdi, hello                  ; Format string pointer
-  mov rax, 0                      ; Clear RAX (no xmm registers used)
-  call printf
-
-  ; Exit the program
-  mov rax, 60                     ; syscall number for exit
-  xor rdi, rdi                    ; Exit status: 0
-  syscall
-
-section .bss
-  section .text
-    extern printf
+          global    main
+          extern    printf
+main:
+	  mov   rdi, fmt
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+fmt: db `Hello, Holberton\n`,0
