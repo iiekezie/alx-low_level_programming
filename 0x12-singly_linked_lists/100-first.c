5550b7850799a2941ceb0a17cae52408d6412dd0
iiekezie <iiekezie@gmail.com>
2023-10-21 08:29:25 -0700

    #advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-/*
- * File: 100-first.c
- * Auth: Ifeanyi I Ekezie
- */
-
-void first(void) __attribute__ ((constructor));
-
 /**
- * pre_main - prints a sentence before the main
- * function is executed
+ * first - function executed before main
+ * Return: no return.
  */
-void pre_main(void)
+
+void __attribute__ ((constructor)) first()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
